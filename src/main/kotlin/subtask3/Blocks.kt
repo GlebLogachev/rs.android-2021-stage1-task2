de44e550e2449127edebeb5322853e0b47bb0a54
GlebLogachev <GlebLogachev@users.noreply.github.com>
2021-06-19 11:27:48 +0700

    feat : add "Blocks" task

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,45 @@
 package subtask3
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
-
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+    //val array = arrayOf(1, 1, 4, "5","qwe", LocalDate.parse("1998-03-14"))
+    return when (blockB) {
+        String::class -> {
+            var sumStr = ""
+            blockA.forEach {
+                if (it is String)
+                    sumStr += it
+            }
+            println(sumStr)
+            return sumStr
+        }
+        Int::class -> {
+            var intSum = 0
+            blockA.forEach {
+                if (it is Int)
+                    intSum += it
+            }
+            println("heelo" + intSum)
+            return intSum
+        }
+        LocalDate::class -> {
+            var currDate: LocalDate = LocalDate.parse("1998-03-14")
+            blockA.forEach {
+                @Suppress("CAST_NEVER_SUCCEEDS")
+                (it as? LocalDate)?.let { date ->
+                    if (date.isAfter(currDate)) {
+                        currDate = date
+                    }
+                }
+            }
+            val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+            return currDate.format(formatter).replace("-",".")
+        }
+        else -> println("такое невозможно")
     }
+}
 }
